87043393d3d797f0656d56fba8dc288a36a897ba
Tibor Bödecs <mail.tib@gmail.com>
2016-05-28 10:37:50 +0200

    iconer 0.3.3

--- iconer.rb
+++ iconer.rb
@@ -2,14 +2,15 @@
 class Iconer < Formula
   desc "App icon generator for Apple platforms."
   homepage "https://github.com/tib/iconer"
-  url "https://github.com/tib/iconer/archive/0.3.2.tar.gz"
-  version "0.3.2"
-  sha256 "a0aea0ff9f300538c949e57c8814b7290d77c853c7b1398d3b8f806a19a70148"
+  url "https://github.com/tib/iconer/archive/0.3.3.tar.gz"
+  version "0.3.3"
+  sha256 "987da2324aec1bff4fa92c4143a2c2f764387a237cb5b6d4e094fd3114a168ad"
 
   def install
     bin.install "bin/iconer"
 
-    prefix.install Dir["resources/*"]
+    prefix.install "resources"
+    # prefix.install Dir["resources/*"]
 
     man1.install "man/iconer.1"
 
