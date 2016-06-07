8f5790eabba8d49d1f99352872799e4285e42a27
Tibor Bödecs <mail.tib@gmail.com>
2016-06-07 21:24:49 +0200

    iconer 0.4.0

--- iconer.rb
+++ iconer.rb
@@ -2,9 +2,9 @@
 class Iconer < Formula
   desc "App icon generator for Apple platforms."
   homepage "https://github.com/tib/iconer"
-  url "https://github.com/tib/iconer/archive/0.3.4.tar.gz"
-  version "0.3.4"
-  sha256 "26ded2834d0e86df2e77b940b1a89a5e244af4e4aa2e4cf9f73be6c1337abc10"
+  url "https://github.com/tib/iconer/archive/0.4.0.tar.gz"
+  version "0.4.0"
+  sha256 "4dae0b2cf17f6ca8f6c3e2d43fda8759e529bc17c5ca1e5633433ff322f88125"
 
   def install
     bin.install "bin/iconer"
